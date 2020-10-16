3c784fdc8121f601b413c824370c8e722e3049ca
Alejandro R. Urzúa <arurz@inaoep.mx>
2020-10-16 17:33:22 -0500

    Updated to native Julia libraries
    I'll keep a legacy record on FrFK-Pycall.jl of the notebook for future reference where I was using the definitions of Hyperfunctions in terms of PyCall wrapper and mpmath libray.
    
    The updated notebook FrFK.jl had the new implementations in terms of SpecialFunctions.jl and HypergeometricFunctions.jl. A substancial speedup is observed.

--- FrFK.jl
+++ FrFK.jl
@@ -7,91 +7,14 @@ using InteractiveUtils
 # ╔═╡ 4b51367c-09ec-11eb-16d9-3dcd2910078e
 begin
 	using PyPlot
-	using PyCall
+	#using PyCall #I'll gonna use SpecialFunctions.jl and HypergeometricFunctions.jl
 	using LaTeXStrings
+	using SpecialFunctions
+	using HypergeometricFunctions
 end
 
 # ╔═╡ eb325bf2-09e3-11eb-3b88-9b0d15626515
 md"## Fractional Fourier-Kravchuk evolution on waveguides"
-
-# ╔═╡ 54542a04-09ec-11eb-1ba0-3b5eb440ac46
-mp = pyimport("mpmath")
-
-# ╔═╡ 7f8aad74-09ec-11eb-3c25-a538f18da2f9
-rc("text", usetex=true)
-
-# ╔═╡ c28078de-09ec-11eb-0c25-a1bd3c7cdd8b
-md" Define the parameters"
-
-# ╔═╡ cb9e6110-09ec-11eb-29bb-b33f49eb026a
-begin
-	N = 32
-	dim = N+1
-	const p = 1/2
-end;
-
-# ╔═╡ 839db44e-09ec-11eb-33e3-01af1f76d1b8
-function nbinom(n,k); # Binomial coefficient in terms of the Gamma function
-    return 1/((n+1)*convert(Float64,mp.beta(k+1, n-k+1)))
-end
-
-# ╔═╡ a73e16c4-09ec-11eb-0f0f-d9772f75fb9c
-function krav(i,j) # Kravchuk symmetric polynomial in terms of 2F1
-    return convert(Float64,mp.hyp2f1(-j,-i,-N,1/p,zeroprec = 250))
-end
-
-# ╔═╡ f0f453ac-09ec-11eb-35bf-5db9aac74d95
-function omega(j)
-    return nbinom(N,j)*(p^j)*(1-p)^(N-j)
-end
-
-# ╔═╡ f2629280-09ec-11eb-34b9-61a08407e9c7
-function h(i)
-    return (((1-p)/p)^i)/(nbinom(N,i))
-end
-
-# ╔═╡ f7b116c6-09ec-11eb-2b95-61db2b1a7196
-function K(i,j)
-    return sqrt(omega(j)/h(i))*krav(i,j)
-end
-
-# ╔═╡ fed998ce-09ec-11eb-2a73-4f900ef78a80
-begin
-	KT = zeros(Float64,(dim,dim))
-	for l in 0:N
-    	for m in 0:N
-        	KT[l+1,m+1] = K(l,m)
-    	end
-	end
-end
-
-# ╔═╡ 4dd56d14-0f64-11eb-03d3-ef32481e399b
-begin
-	fig2 = figure(figsize=(5,5))
-	ax2 = gca()
-	ax2.imshow(KT, cmap="gray")
-	title(L"\textbf{Kravchuk Matrix}\; K(i,j)\; \textbf{(The trilobite)}")
-	ax2.set_xlabel(L"\textbf{Pseudo Energy Level:}\; 0\leq i\leq N")
-	ax2.set_ylabel(L"\textbf{Position Index:}\; 0\leq j\leq N")
-	ax2.tick_params(direction="out",length=5,width=2,labelsize=10)
-	ax2.set_xticks(0:8:N, minor = false)
-	ax2.set_yticks(0:8:N, minor = false)
-	
-	axins1 = ax2.inset_axes([0, 0.8, 0.2, 0.2])
-	axins1.plot(KT[1,:],"k:",linewidth=1)
-	axins1.set_xticklabels("")
-	axins1.set_yticklabels("")
-	axins1.text(7.5,0.01,L"K(0,j)",fontsize=9)
-	
-	axins2 = ax2.inset_axes([0.8, 0, 0.2, 0.2])
-	axins2.plot(KT[N+1,:],"k:",linewidth=1)
-	axins2.set_xticklabels("")
-	axins2.set_yticklabels("")
-	axins2.text(15,-0.35,L"K(N,j)",fontsize=9)
-	
-	tight_layout()
-	#savefig("trilo.png",dpi=300,transparent=true)
-end
 
 # ╔═╡ 74a12d8e-0f69-11eb-0650-e79c06fd1080
 md"Kravchuk matrix is defined as
@@ -107,28 +30,106 @@ $k(i,j)=~_{2}F_{1}(-j,-i,-N,1/p).$
 Note: When $p=1/2$, the symmetric Kravchuk polynomials are obtained, henceforth, the symmetric Kravchuk functions follows.
 "
 
+# ╔═╡ 7f8aad74-09ec-11eb-3c25-a538f18da2f9
+begin
+	rc("text", usetex=true)
+	ion()
+end
+
+# ╔═╡ c28078de-09ec-11eb-0c25-a1bd3c7cdd8b
+md" Define the parameters"
+
+# ╔═╡ cb9e6110-09ec-11eb-29bb-b33f49eb026a
+begin
+	N = 32
+	dim = N+1
+	const p = 1/2.0
+end;
+
+# ╔═╡ bb1e9a12-0fe6-11eb-0551-137403aecef6
+function bspecial(n,k)
+	return 1/((n+1)*beta(k+1,n-k+1))
+end;
+
+# ╔═╡ ee7b88a2-0fe6-11eb-3465-8b70e58f07c1
+function kravs(i,j)
+	return HypergeometricFunctions._₂F₁maclaurin(-j,-i,-N,1/p)
+end;
+
+# ╔═╡ 4f876a76-0fe7-11eb-3f46-0f18d733b446
+function omegas(j)
+	return bspecial(N,j)*(p^j)*(1-p)^(N-j)
+end;
+
+# ╔═╡ 590a100a-0fe7-11eb-017c-4d4ff299ed8a
+function hs(i)
+	return (((1-p)/p)^i)/(bspecial(N,i))
+end;
+
+# ╔═╡ a5f2bf3c-0fe7-11eb-3377-8d5228bb6476
+function Ks(i,j)
+	return sqrt(omegas(j)/hs(i))*kravs(i,j)
+end;
+
+# ╔═╡ d01de678-0fe8-11eb-2831-89f652e45a9a
+begin
+	KTs = zeros(Float64,(dim,dim))
+	@timed for l in 0:N
+    	for m in 0:N
+        	KTs[l+1,m+1] = Ks(l,m)
+    	end
+	end
+end
+
+# ╔═╡ 4692817e-0fe9-11eb-22e0-dfdab6509141
+begin	
+	fig1 = figure(figsize=(5,5))
+	ax1 = gca()
+	ax1.imshow(KTs, cmap="gray")
+	ax1.set_title(L"\textbf{Kravchuk Matrix}\; K(i,j)\;\textbf{(The trilobite)}")
+	ax1.set_xlabel(L"\textbf{Pseudo Energy Level:}\; 0\leq i\leq N")
+	ax1.set_ylabel(L"\textbf{Position Index:}\; 0\leq j\leq N")
+	ax1.tick_params(direction="out",length=5,width=2,labelsize=10)
+	ax1.set_xticks(0:8:N, minor = false)
+	ax1.set_yticks(0:8:N, minor = false)
+	
+	axins1a = ax1.inset_axes([0, 0.8, 0.2, 0.2])
+	axins1a.plot(KTs[1,:],"k:",linewidth=1)
+	axins1a.set_xticklabels("")
+	axins1a.set_yticklabels("")
+	axins1a.text(7.5,0.01,L"K(0,j)",fontsize=9)
+	
+	axins2a = ax1.inset_axes([0.8, 0, 0.2, 0.2])
+	axins2a.plot(KTs[N+1,:],"k:",linewidth=1)
+	axins2a.set_xticklabels("")
+	axins2a.set_yticklabels("")
+	axins2a.text(15,-0.35,L"K(N,j)",fontsize=9)
+	
+	tight_layout()
+	savefig("trilo.png",dpi=300,transparent=true)
+end
+
 # ╔═╡ 33089668-09ed-11eb-30db-8f33d969a115
 md"
-October 9
+October 16
 
-Status: Kravchuk matrix completed
+Status: Kravchuk matrix completed. Changed definitions of functions using native Julia libraries
 
 Next: Write the fractional evolution, and plotting the results
 "
 
 # ╔═╡ Cell order:
 # ╠═eb325bf2-09e3-11eb-3b88-9b0d15626515
+# ╟─74a12d8e-0f69-11eb-0650-e79c06fd1080
 # ╠═4b51367c-09ec-11eb-16d9-3dcd2910078e
-# ╟─54542a04-09ec-11eb-1ba0-3b5eb440ac46
 # ╠═7f8aad74-09ec-11eb-3c25-a538f18da2f9
 # ╟─c28078de-09ec-11eb-0c25-a1bd3c7cdd8b
 # ╠═cb9e6110-09ec-11eb-29bb-b33f49eb026a
-# ╠═839db44e-09ec-11eb-33e3-01af1f76d1b8
-# ╠═a73e16c4-09ec-11eb-0f0f-d9772f75fb9c
-# ╠═f0f453ac-09ec-11eb-35bf-5db9aac74d95
-# ╠═f2629280-09ec-11eb-34b9-61a08407e9c7
-# ╠═f7b116c6-09ec-11eb-2b95-61db2b1a7196
-# ╠═fed998ce-09ec-11eb-2a73-4f900ef78a80
-# ╠═4dd56d14-0f64-11eb-03d3-ef32481e399b
-# ╟─74a12d8e-0f69-11eb-0650-e79c06fd1080
+# ╠═bb1e9a12-0fe6-11eb-0551-137403aecef6
+# ╠═ee7b88a2-0fe6-11eb-3465-8b70e58f07c1
+# ╠═4f876a76-0fe7-11eb-3f46-0f18d733b446
+# ╠═590a100a-0fe7-11eb-017c-4d4ff299ed8a
+# ╠═a5f2bf3c-0fe7-11eb-3377-8d5228bb6476
+# ╠═d01de678-0fe8-11eb-2831-89f652e45a9a
+# ╠═4692817e-0fe9-11eb-22e0-dfdab6509141
 # ╟─33089668-09ed-11eb-30db-8f33d969a115
