a889b15ed1073a75ee204facc20a2424c0324fe2
arurz <arurz@inaoep.mx>
2020-12-29 00:29:26 -0600

    Jx_Coupling added
    Minimal script of Jx coupling added

---
+++ tests/JxCoupling.jl
@@ -0,0 +1,21 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0b0902da-40f0-11eb-3e10-a1fe1bd1f364
+begin
+	using SpecialPolynomials
+	using PyPlot
+end
+
+# ╔═╡ 0b507856-40f1-11eb-244f-f15c1038b96f
+begin
+	rc("text", usetex = true)
+	ion()
+end
+
+# ╔═╡ Cell order:
+# ╠═0b0902da-40f0-11eb-3e10-a1fe1bd1f364
+# ╠═0b507856-40f1-11eb-244f-f15c1038b96f
