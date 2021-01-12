f5ae4254a23f730c5cd915325211031580313259
rurz <arurz@inaoep.mx>
2021-01-11 21:40:26 -0600

    Added Ankara.jl

--- tests/JxCoupling.jl
+++ tests/JxCoupling.jl
@@ -1,13 +1,14 @@
 ### A Pluto.jl notebook ###
-# v0.12.17
+# v0.12.18
 
 using Markdown
 using InteractiveUtils
 
 # ╔═╡ 0b0902da-40f0-11eb-3e10-a1fe1bd1f364
 begin
-	using SpecialPolynomials
 	using PyPlot
+	using SpecialFunctions
+	using HypergeometricFunctions
 end
 
 # ╔═╡ 0b507856-40f1-11eb-244f-f15c1038b96f
@@ -16,6 +17,17 @@ begin
 	ion()
 end
 
+# ╔═╡ eaadeff2-546b-11eb-1289-9bade75bad45
+function J(α, β, n, z)
+    T = eltype(z/2)
+    tot = zero(T)
+    for k in 0:n
+        tot += binomial(α + n, k) * binomial(β + n, n - k) * ((z + 1)/2)^k * ((z - 1)/2)^(n - k)
+    end
+    tot
+end;
+
 # ╔═╡ Cell order:
 # ╠═0b0902da-40f0-11eb-3e10-a1fe1bd1f364
 # ╠═0b507856-40f1-11eb-244f-f15c1038b96f
+# ╠═eaadeff2-546b-11eb-1289-9bade75bad45
